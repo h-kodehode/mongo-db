31f3da63871c0d160a3b16107e17b2a50f60a544
Helez Baban <helez25sfj@gmail.com>
2025-10-16 14:01:38 +0200

    fixed small error in api route

--- app/api/mongoFetch/route.js
+++ app/api/mongoFetch/route.js
@@ -3,10 +3,7 @@ import { mongoDBConnect } from "@/lib/mongodbConnect";
 export async function GET() {
   const databaseConnection = await mongoDBConnect();
 
-  const data = await databaseConnection
-    .collection("firstDB")
-    .find({})
-    .toArray();
+  const data = await databaseConnection.collection("users").find({}).toArray();
 
   return Response.json(data);
 }
